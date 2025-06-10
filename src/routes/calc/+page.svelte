b2e9e0f74a6e9960f3c2df0b57b29c38164d00e1
mikael-silva77 <mkaelsoares9@gmail.com>
2025-06-10 08:05:38 -0400

    vineado

--- src/routes/calc/+page.svelte
+++ src/routes/calc/+page.svelte
@@ -11,7 +11,8 @@
                 .replace(/√\(/g, 'Math.sqrt(')
                 .replace(/(\d+)%/g, '($1*0.01)')
                 .replace(/(\d+)\²/g, '($1**2)')
-                .replace(/log\(/g, 'Math.log10(');
+                .replace(/log\(/g, 'Math.log10(')
+                .replace(/ mod /g, '%');
             display = eval(expr);
         } catch {
             display = 'Erro';
@@ -35,7 +36,6 @@
     }
 
     function square() {
-        // aplica ao último número digitado
         const match = display.match(/(\d+)(?!.*\d)/);
         if (match) {
             display = display.slice(0, match.index + match[0].length) + '²';
@@ -45,6 +45,27 @@
     function log10() {
         display += 'log(';
     }
+
+    function invert() {
+        try {
+            let result = eval(display);
+            display = (-result).toString();
+        } catch {
+            display = 'Erro';
+        }
+    }
+
+    function roundNumber() {
+        try {
+            let result = eval(display);
+            display = Math.round(result).toString();
+        } catch {
+            display = 'Erro';
+        }
+    }
+    function modulo() {
+    display += ' mod ';
+}
 </script>
 
 <div class="text-center mt-3">
@@ -56,34 +77,46 @@
                 <td><button class="btn btn-primary w-100" onclick={() => press('(')}>&lpar;</button></td>
                 <td><button class="btn btn-primary w-100" onclick={() => press(')')}>&rpar;</button></td>
                 <td><button class="btn btn-primary w-100" onclick={() => press('/')}>/</button></td>
+               
             </tr>
 
             <tr>
                 <td><button class="btn btn-primary w-100" onclick={() => sqrt()}>√</button></td>
-                <td><button class="btn btn-primary w-100" onclick={() => percent()}>%</button></td>
                 <td><button class="btn btn-primary w-100" onclick={() => square()}>x²</button></td>
                 <td><button class="btn btn-primary w-100" onclick={() => log10()}>log</button></td>
+                <td><button class="btn btn-primary w-100" onclick={() => percent()}>%</button></td>
+
             </tr>
 
             <tr>
-                <td><button class="btn btn-success w-100" onclick={() => press(7)}>7</button></td>
-                <td><button class="btn btn-success w-100" onclick={() => press(8)}>8</button></td>
-                <td><button class="btn btn-success w-100" onclick={() => press(9)}>9</button></td>
-                <td><button class="btn btn-primary w-100" onclick={() => press('*')}>x</button></td>
+            <td><button class="btn btn-primary w-100" onclick={() => invert()}>±</button></td>
+            <td><button class="btn btn-primary w-100" onclick={() => roundNumber()}>⭮</button></td> 
+            <td><button class="btn btn-primary w-100" onclick={() => modulo()}>mod</button></td>
+            <td><button class="btn btn-primary w-100" onclick={() => press('*')}>x</button></td>
+        </tr>
+
+            <tr>
+        
+                    <td><button class="btn btn-success w-100" onclick={() => press(7)}>7</button></td>
+                    <td><button class="btn btn-success w-100" onclick={() => press(8)}>8</button></td>
+                    <td><button class="btn btn-success w-100" onclick={() => press(9)}>9</button></td>
+                    <td><button class="btn btn-primary w-100" onclick={() => press('-')}>-</button></td>
+            
+          
             </tr>
 
             <tr>
                 <td><button class="btn btn-success w-100" onclick={() => press(4)}>4</button></td>
                 <td><button class="btn btn-success w-100" onclick={() => press(5)}>5</button></td>
                 <td><button class="btn btn-success w-100" onclick={() => press(6)}>6</button></td>
-                <td><button class="btn btn-primary w-100" onclick={() => press('-')}>-</button></td>
+                <td style="height:0" rowspan=2><button class="btn btn-primary w-100 h-100" onclick={() => press('+')}>+</button></td>
             </tr>
 
             <tr>
                 <td><button class="btn btn-success w-100" onclick={() => press(1)}>1</button></td>
                 <td><button class="btn btn-success w-100" onclick={() => press(2)}>2</button></td>
                 <td><button class="btn btn-success w-100" onclick={() => press(3)}>3</button></td>
-                <td><button class="btn btn-primary w-100" onclick={() => press('+')}>+</button></td>
+          
             </tr>
 
             <tr>
